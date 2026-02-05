0aa19702ac6768644a82966817a158a0f97ffdfb
mathew Kasinde <officialeric994@gmail.com>
2026-02-05 21:00:00 +0300

    Update PageHeader and globals.css

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -3,11 +3,19 @@ import Link from "next/link";
 interface PageHeaderProps {
   title: string;
   currentPage: string;
+  image?: string;
 }
 
-export default function PageHeader({ title, currentPage }: PageHeaderProps) {
+export default function PageHeader({ title, currentPage, image = "/img/carousel-1.jpg" }: PageHeaderProps) {
   return (
-    <div className="container-fluid bg-primary py-5" style={{ marginBottom: 90 }}>
+    <div
+      className="page-header-hero container-fluid py-5"
+      style={{
+        marginBottom: 90,
+        background: `linear-gradient(rgba(9, 30, 62, 0.85), rgba(9, 30, 62, 0.85)), url(${image}) center center no-repeat`,
+        backgroundSize: "cover",
+      }}
+    >
       <div className="row py-5">
         <div className="col-12 pt-lg-5 mt-lg-5 text-center">
           <h1 className="display-4 text-white">{title}</h1>
